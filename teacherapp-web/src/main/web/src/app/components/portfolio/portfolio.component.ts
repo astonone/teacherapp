ad6620ffdb05bb03bba8bb1efc13428f66e03aab
vitynya <viktor.kuligyn@smartbics.com>
2019-08-14 12:17:47 +0300

    1) security problems were fixed 2) prototype of home and portfolio pages were improved

---
+++ teacherapp-web/src/main/web/src/app/components/portfolio/portfolio.component.ts
@@ -0,0 +1,25 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SharedService } from '../../services/shared.service';
+import { User } from '../../dto/user';
+
+@Component({
+    selector: 'home',
+    templateUrl: './portfolio.component.html',
+    styleUrls: [
+        './portfolio.component.css'
+    ]
+})
+
+export class PortfolioComponent implements OnInit {
+
+    public user: User;
+
+    constructor(private router: Router,
+                private shared: SharedService) {
+        this.user = this.shared.createEmptyUserStub();
+    }
+
+    ngOnInit() {
+    }
+}
